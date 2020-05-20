d317be14778f0486ddee19eb36490641816534d8
Thibault <dudouit.thibault@outlook.com>
2020-05-20 17:44:52 +0200

    HttpOptions

--- src/app/app-config.service.ts
+++ src/app/app-config.service.ts
@@ -8,8 +8,15 @@ export class AppConfigService {
 
   public url: string = "http://176.143.99.66:8080/api";
   public httpOptions : Object = null;
+  public login;
+  public mdp;
 
   constructor() {
-  
+    let myHeaders: HttpHeaders = new HttpHeaders();
+    //myHeaders = myHeaders.append('Authorization', 'Basic ' + btoa(login +":"+ mdp));
+    myHeaders = myHeaders.append('Authorization', 'Basic ' + btoa('flo:mdp'));
+    this.httpOptions = { headers: myHeaders};
    }
+
+
 }
